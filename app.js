c93b7ac8512292f89d28f47e7a4aa26ddcf5151d
Maycom Willams <74081416+Maycomwill@users.noreply.github.com>
2024-01-06 06:04:19 -0300

    fix(dotenv-e-cors-fixed): Corrigido forma de importação do cors, adicionado configuração para ler variáveis de ambientes no app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import fastify from "fastify";
-import productRoutes from './src/routes/productRoutes.js'
-import { cors } from '@fastify/cors';
+import productRoutes from "./src/routes/productRoutes.js";
+import cors from "@fastify/cors";
+import "dotenv/config";
 import { prisma } from "./lib/database.js";
 
 const app = fastify({ logger: true });
@@ -9,20 +10,23 @@ app.post("/create-user", async (request, reply) => {
   const query = await prisma.user.create({
     data: {
       name: request.body.name,
-      email: request.body.email
-    }
-  })
+      email: request.body.email,
+    },
+  });
   return reply.status(201).send(query);
 });
 
 app.register(productRoutes, { prefix: "/products" });
 
-app.register(cors, { origin: '*' });
+app.register(cors, { origin: "*" });
 
-app.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
+app.listen(
+  { port: process.env.PORT | 3000, host: process.env.HOST | "0.0.0.0" },
+  (err, address) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+    app.log.info(`server listening on ${address}`);
   }
-  app.log.info(`server listening on ${address}`);
-});
+);
