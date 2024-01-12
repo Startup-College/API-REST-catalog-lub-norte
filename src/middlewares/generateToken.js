261aac719a839a3c8259776e6128fe953e76b437
dev-edsonlopes <dev_edson@outlook.com>
2024-01-12 20:12:26 -0300

    feat(Gerando token): Definindo expiração de Tocken

--- src/middlewares/generateToken.js
+++ src/middlewares/generateToken.js
@@ -3,7 +3,7 @@ import JWT from "jsonwebtoken";
 
 export function generateToken(payload) {
   return new Promise((resolve, reject) => {
-    JWT.sign(payload, process.env.SECRET_KEY, { algorithm: 'HS256' }, (err, token) => {
+    JWT.sign(payload, process.env.SECRET_KEY, { algorithm: 'HS256', expiresIn: '1d'}, (err, token) => {
       if (err) {
         reject(new Error('ERR_INVALID_TOKEN'));
       } else {
